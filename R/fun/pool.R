f48d0902a58bbb9fa3dea2712c11ad50ada0a6d7
Vision9423 <nordeb_q@yahoo.com>
2025-09-13 22:08:44 +0900

    Randomization App

--- R/fun/pool.R
+++ R/fun/pool.R
@@ -1,6 +1,6 @@
 db_host <- Sys.getenv("db_host")
 db_port <- Sys.getenv("db_port")
-db_name <- Sys.getenv("db_dbname")
+db_dbname <- Sys.getenv("db_dbname")
 db_user <- Sys.getenv("db_user")
 db_password <- Sys.getenv("db_password")
 db_table_cred <- Sys.getenv("db_table_cred")
@@ -10,7 +10,7 @@ pool <- dbPool(
   drv = MariaDB(),
   host = db_host,
   port = db_port,
-  dbname = db_name,
+  dbname = db_dbname,
   user = db_user,
   password = db_password,
   maxSize = 10
